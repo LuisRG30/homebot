88253895fe883ae3c635b7486a6a93f151472e91
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-08-06 15:38:20 -0500

    indicated views are in welcome

--- welcome/urls.py
+++ welcome/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+
+from django.contrib import admin
+
+admin.autodiscover()
+
 
 from . import views
 urlpatterns = [
-    path("", views.index, name = "index")
+    path("", welcome.views.index, name = "index")
 ]
