40d9df0025ab19b14ac9ed5d063d675e1684842b
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-08-06 22:26:29 -0500

    returning to simple import

--- welcome/urls.py
+++ welcome/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 app_name = "welcome"
-import welcome.views
+from welcome import views
 urlpatterns = [
     path("", views.index, name = "index")
 ]
