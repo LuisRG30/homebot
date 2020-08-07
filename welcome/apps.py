8969c5a0740b09502dbd262fb76e563b74609ffb
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-08-06 20:05:54 -0500

    appname testing

---
+++ welcome/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Welcome(AppConfig):
+    name = 'welcome'
--- welcome/urls.py
+++ welcome/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-
+app_name = "welcome"
 from . import views
 urlpatterns = [
     path("", views.index, name = "index")
