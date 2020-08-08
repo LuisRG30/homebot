20e0b1b6174a00b43eb9e950725214968015a0a5
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-08-08 14:06:43 -0500

    no mayor changes

--- welcome/models.py
+++ welcome/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField()
-    password = models.CharField()
+    username = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
