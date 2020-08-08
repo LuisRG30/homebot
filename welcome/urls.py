4bf48e91785c1d64166717bc7acbcb7e28225abf
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-08-08 11:16:00 -0500

    added fees form and styling

--- welcome/urls.py
+++ welcome/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 app_name = "welcome"
 from . import views
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("cotiza", views.fee, name="fee")
 ]
