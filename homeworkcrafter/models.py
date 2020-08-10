4a532a1e872423b773459d7d1b57bd3d6bd7ccb9
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-08-10 10:39:46 -0500

    simple db model for homework

--- homeworkcrafter/models.py
+++ homeworkcrafter/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Homework(models.Model):
-    name = models.CharField(max_length=64)
+    homework = models.CharField(max_length=64)
     email = models.CharField(max_length=64)
     number = models.CharField(max_length=32)
     subject = models.CharField(max_length=64)
@@ -11,4 +11,4 @@ class Homework(models.Model):
     price = models.IntegerField(blank=True, default=0)
 
     def __str__(self):
-        return f"[{self.name}, {self.subject}, {self.date}, {self.price}] "
+        return f"[{self.homework}, {self.subject}, {self.date}, {self.price}] "
